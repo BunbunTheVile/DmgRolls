063ad6a2f730b21e1f251a0acbd9cd431228ba3d
Bunny <evolvingbunny@gmail.com>
2022-02-16 15:13:43 +0100

    added automatic validation and adjustment of input values

--- DmgRolls/MainWindow.xaml.cs
+++ DmgRolls/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using DmgRolls.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
@@ -6,6 +7,12 @@ using System.Windows.Controls;
 /*
  * As I understand it, this code-behind basically functions as the controller?
  * Well, I'll use it as one, for what it's worth.
+ * 
+ * TODO:
+ *  -lostfocus modifier event
+ *  -lower bound events
+ *  -upper bound events
+ *  -calculate events
  */
 
 namespace DmgRolls
@@ -13,15 +20,16 @@ namespace DmgRolls
     public partial class MainWindow : Window
     {
         public List<DiceRow> diceRows = new List<DiceRow>();
+        private int staticModifier;
+        private int lowerBound;
+        private int upperBound;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            AddDie(4, 4);
-            AddDie(3, 6);
-
-
+            AddDie(2, 6);
+            AdjustAndReadInputFields();
         }
 
         private void AddDie(int diceCount, int diceType)
@@ -72,9 +80,106 @@ namespace DmgRolls
             diceRows.RemoveAt(rowIndex);
         }
 
+        private Dictionary<string, bool> ValidateInputFields()
+        {
+            bool isValidStaticModifier = int.TryParse(StaticModifierBox.Text, out this.staticModifier);
+            bool isValidLowerBound = int.TryParse(LowerBoundBox.Text, out this.lowerBound);
+            bool isValidUpperBound = int.TryParse(UpperBoundBox.Text, out this.upperBound);
+            bool isValidDiceInput = true;
+
+            foreach(DiceRow row in diceRows)
+            {
+                if (!int.TryParse(row.diceCountBox.Text, out _) || !int.TryParse(row.diceTypeBox.Text, out _))
+                {
+                    isValidDiceInput = false;
+                    break;
+                }
+            }
+
+            var results = new Dictionary<string, bool>();
+            results.Add("staticModifier", isValidStaticModifier);
+            results.Add("lowerBound", isValidLowerBound);
+            results.Add("upperBound", isValidUpperBound);
+            results.Add("diceInput", isValidDiceInput);
+            return results;
+        }
+
+        private void AdjustAndReadInputFields()
+        {
+            //set the dice counts to 1 if they don't hold a valid number
+            //set the dice types to 6 if they don't hold a valid number
+            //set the modifier to 0 if it doesn't hold a valid number
+            //set the bounds to min or max if they don't hold a valid number
+            //set the bounds to min or max if they are too low or too high
+
+            Dictionary<string, bool> validities = ValidateInputFields();
+
+            if (!validities["diceInput"])
+            {
+                foreach (DiceRow row in diceRows)
+                {
+                    if (!int.TryParse(row.diceCountBox.Text, out _)) 
+                    {
+                        row.diceCountBox.Text = "1";
+                    }
+                    if (!int.TryParse(row.diceTypeBox.Text, out _))
+                    {
+                        row.diceTypeBox.Text = "6";
+                    }
+                }
+            }
+
+            if (!validities["staticModifier"])
+            {
+                this.staticModifier = 0;
+                StaticModifierBox.Text = "0";
+            }
+
+            int minRoll = this.staticModifier;
+            int maxRoll = this.staticModifier;
+            foreach (DiceRow row in diceRows)
+            {
+                minRoll += int.Parse(row.diceCountBox.Text);
+                maxRoll += int.Parse(row.diceCountBox.Text) * int.Parse(row.diceTypeBox.Text);
+            }
+
+            if (!validities["lowerBound"])
+            {
+                lowerBound = minRoll;
+                LowerBoundBox.Text = minRoll.ToString();
+            }
+            if (!validities["upperBound"])
+            {
+                upperBound = maxRoll;
+                UpperBoundBox.Text = maxRoll.ToString();
+            }
+
+            if (int.Parse(LowerBoundBox.Text) < minRoll)
+            {
+                lowerBound = minRoll;
+                LowerBoundBox.Text = minRoll.ToString();
+            }
+            if (int.Parse(LowerBoundBox.Text) > maxRoll)
+            {
+                lowerBound = maxRoll;
+                LowerBoundBox.Text = maxRoll.ToString();
+            }
+            if (int.Parse(UpperBoundBox.Text) < minRoll)
+            {
+                upperBound = minRoll;
+                UpperBoundBox.Text = minRoll.ToString();
+            }
+            if (int.Parse(UpperBoundBox.Text) > maxRoll)
+            {
+                upperBound = maxRoll;
+                UpperBoundBox.Text = maxRoll.ToString();
+            }
+        }
+
         public void PlusButton_Click(object sender, RoutedEventArgs e)
         {
             AddDie(1, 6);
+            AdjustAndReadInputFields();
         }
 
         public void MinusButton_Click(object sender, RoutedEventArgs e)
@@ -82,6 +187,27 @@ namespace DmgRolls
             Button button = (Button)sender;
             int row = Grid.GetRow(button);
             RemoveDie(row);
+            AdjustAndReadInputFields();
+        }
+
+        private void StaticModifierBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            AdjustAndReadInputFields();
+        }
+
+        private void lowerBoundBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            AdjustAndReadInputFields();
+        }
+
+        private void UpperBoundBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            AdjustAndReadInputFields();
+        }
+
+        public void diceField_LostFocus(object sender, RoutedEventArgs e)
+        {
+            AdjustAndReadInputFields();
         }
     }
 }
